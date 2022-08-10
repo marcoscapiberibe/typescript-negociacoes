ae56025e86090d6102993ca459219e9af4b9729b
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-08-10 20:50:52 -0300

    refatoramento do constructor no arquivo negociacao.ts usando atalho do TypeScript
    refatoramento do constructor no arquivo negociacao.ts usando atalho do TypeScript

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,13 +1,18 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date, 
+        private _quantidade: number, 
+        private _valor: number) {}
+    //     {
+    //     this._data = data;
+    //     this._quantidade = quantidade;
+    //     this._valor = valor;
+    // }
+    
 
     get data(): Date {
         return this._data;
