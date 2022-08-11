3ffb355181a401fb585b523c05cfbb0e41332162
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-08-10 21:06:45 -0300

    refatoramento do Array e ReadonlyArray  no arquivo negociacoes.ts usando atalho do TypeScript
    refatoramento do Array e ReadonlyArray  no arquivo negociacoes.ts usando atalho do TypeScript

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,14 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    // private negociacoes: Array<Negociacao> = [];  <-- mesma coisa que:
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
